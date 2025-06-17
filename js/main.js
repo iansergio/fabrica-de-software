28104e9baf6c7bd2ed75952ca4580fd055cbcc97
Ian Sergio Helfenberger <iansergiohelf@gmail.com>
2025-06-17 18:46:21 -0300

    navbar with responsive section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-function openNav() {
+const openNav = () => {
     document.getElementById("sidenav").style.width = "250px";
 }
 
-function closeNav() {
+const closeNav = () => {
     document.getElementById("sidenav").style.width = "0";
+}
+
+const loadPage = (page, element) => {
+    fetch(page)
+        .then(res => {
+            if (!res.ok) throw new Error("Página não encontrada")
+            return res.text()
+        })
+        .then(html => {
+            document.getElementById("content").innerHTML = html;
+        })
+        .catch(err => {
+            document.getElementById("content").innerHTML = "<p>Error loading page.</p>"
+        })
+
+    const links = document.querySelectorAll(".nav-links a")
+    links.forEach(link => link.classList.remove("active"))
+
+    element.classList.add("active");
+
 }
